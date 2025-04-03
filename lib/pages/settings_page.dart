7e74584619edd8416b1543d93cd5e1d447233116
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-03 17:42:36 +0500

    imp

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -10,6 +11,16 @@ class SettingsPage extends StatelessWidget {
       body: Stack(
         children: [
           // Background Image
+          Row(
+            children: [
+              Expanded(
+                child: Image.asset(
+                  "assets/background.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
 
           SafeArea(
             child: Padding(
@@ -18,10 +29,68 @@ class SettingsPage extends StatelessWidget {
                 child: Column(
                   children: [
                     // Settings
+                    Row(
+                      children: [
+                        Icon(CupertinoIcons.chevron_left),
+                        Spacer(),
+                        Text(
+                          "Settings",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Spacer(),
+                        Icon(Icons.menu),
+                      ],
+                    ),
 
                     // Avatar
+                    SizedBox(height: 40),
+                    Image.asset(
+                      "assets/avatar.png",
+                      height: 140,
+                      width: 140,
+                    ),
 
                     // Edit Profile & Share Profile
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: CupertinoButton(
+                            color: Colors.white,
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.edit),
+                                SizedBox(width: 10),
+                                Text("Edit Profile"),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Expanded(
+                          child: CupertinoButton(
+                            color: Colors.white,
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.share),
+                                SizedBox(width: 10),
+                                Text("Share Profile"),
+                              ],
+                            ),
+                          ),
+                        ),
+
+
+
+                      ],
+                    ),
 
                     // Basic Setting (3x)
 
